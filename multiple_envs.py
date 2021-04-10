42b01bcbd9480ceb9ad4b30b2dbd1f93290fe525
nvsclub <720limited@live.com.pt>
2021-04-10 10:39:19 +0100

    Testing multiple environments

--- multiple_envs.py
+++ multiple_envs.py
@@ -19,6 +19,7 @@ import numpy as np
 import lib.draw as draw
 import matplotlib.pyplot as plt
 from tqdm import tqdm
+import time
 
 def make_env(rank, seed=0):
     def _init():
@@ -28,24 +29,24 @@ def make_env(rank, seed=0):
     set_random_seed(seed)
     return _init
 
-env = SubprocVecEnv([make_env(i) for i in range(1)])
+if __name__ == '__main__':
+    env = SubprocVecEnv([make_env(i) for i in range(1)])
 
-n_actions = env.action_space.shape[-1]
+    n_actions = env.action_space.shape[-1]
 
-import time
-t1 = time.time()
+    t1 = time.time()
 
-model = A2C("MlpPolicy", env, verbose=1)
-model.learn(total_timesteps=25000, log_interval=10000)
-print(time.time() - t1)
+    model = A2C('MlpPolicy', env, verbose=1)
+    model.learn(total_timesteps=25000, log_interval=10000)
+    print(time.time() - t1)
 
-saving_rewards = []
-obs = env.reset()
-for i in tqdm(range(10000)):
-  action, _states = model.predict(obs)
-  obs, rewards, done, info = env.step(action)
-  #env.render()
-  if done:
-      saving_rewards.append(rewards)
-      env.reset()
-np.mean(saving_rewards)    
+    saving_rewards = []
+    obs = env.reset()
+    for i in tqdm(range(10000)):
+        action, _states = model.predict(obs)
+        obs, rewards, done, info = env.step(action)
+        #env.render()
+        if done:
+            saving_rewards.append(rewards)
+            env.reset()
+    np.mean(saving_rewards)    
